274a7c6b715f16e59771a381956cd664b82b0567
Daniel Zych <danjzych@gmail.com>
2023-11-15 15:23:20 -0600

    Added styles for navbar when logged out, and cleaned up some redundant code

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -6,23 +6,27 @@ import './Navbar.css'
 function Navbar({ logout }) {
     const { user } = useContext(userContext);
 
+    const navLinkClass = "px-2 hover:opacity-80";
+
 
     return(<nav className="navbar fixed text-base-100 border-b-2 border-b-neutral-600 bg-gradient-to-r from-cyan-500 to-blue-500 z-50" >
             {!user && <>
-                <NavLink to='/login' className="hover:opacity-80">Login</NavLink>
-                <NavLink to='/signup' className="hover:opacity-80">Signup</NavLink>
-            <Link to="/"><h1 className="text-4xl navbar-center font-extrabold text-primary">Friender</h1></Link>
+            <Link to="/" className="navbar-start"><h1 className="px-2 text-4xl font-extrabold text-primary">Friender</h1></Link>
+            <div className="navbar-end">
+                <NavLink to='/login' className={navLinkClass}>Login</NavLink>
+                <NavLink to='/signup' className={navLinkClass}>Signup</NavLink>
+            </div>
             </>}
             {user &&
             <>
             <div className="navbar-start">
-                <NavLink to='/profile' className="px-2 hover:opacity-80">Profile</NavLink>
-                <button onClick={logout} className="px-2 hover:opacity-80">Logout</button>
+                <NavLink to='/profile' className={navLinkClass}>Profile</NavLink>
+                <button onClick={logout} className={navLinkClass}>Logout</button>
             </div>
             <Link to="/"><h1 className='text-4xl navbar-center font-extrabold text-primary'>Friender</h1></Link>
             <div className="navbar-end">
-                <NavLink to="/people" className="px-2 hover:opacity-80">Find Friends</NavLink>
-                <NavLink to="/matches" className="px-2 hover:opacity-80">Matches</NavLink>
+                <NavLink to="/people" className={navLinkClass}>Find Friends</NavLink>
+                <NavLink to="/matches" className={navLinkClass}>Matches</NavLink>
             </div>
             </>
             }
