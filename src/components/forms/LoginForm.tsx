049822f614120814730ba88759af73f0a8e832b3
Daniel Zych <danjzych@gmail.com>
2023-11-15 21:57:34 -0600

    added styles for login form

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,4 +1,3 @@
-import "./LoginForm.css";
 import React, { useState } from "react";
 import { LoginInterface } from "../../types/interfaces";
 
@@ -28,26 +27,34 @@ function LoginForm({ handleSubmit }: LoginFormProps) {
 
 
   return (
-    <>
-      <form className="LoginForm" onSubmit={submitForm}>
-        <div className="form-input-container">
-          <label htmlFor="username">Username</label>
+    <div className="position absolute top-16 w-full flex justify-center">
+    <form className="flex flex-col justify-between items-center mx-auto mt-20 py-4 h-96 w-96 min-h-full border-0 border-base-200 rounded-xl shadow-2xl" onSubmit={submitForm}>
+      <div className="w-full text-center">
+      <h2 className="text-2xl font-bold text-secondary">Login</h2>
+      <h3 className="text-md font-light text-accent italic">Welcome back!</h3>
+      </div>
+      <div className="w-full">
+        <div className="w-100 mb-6">
+          <label htmlFor="username" className="block px-4 font-semilight mb-2">Username:</label>
           <input type="text" onChange={handleChange}
             name="username"
             value={formData.username}
+            placeholder="user@email.com"
+            className="border-2 border-base-300 rounded-md bg-gray-50 w-11/12 mx-4 px-1 py-0.5 font-extralight text-gray-600 active:border-blue-500 placeholder:italic placeholder:text-gray-500"
           />
         </div>
-
-        <div className="form-input-container">
-          <label htmlFor="password">Password</label>
+        <div className="w-100 mb-6">
+          <label htmlFor="password" className="block px-4 font-semilight mb-2">Password:</label>
           <input type="password" onChange={handleChange}
             name="password"
             value={formData.password}
+            className="border-2 border-base-300 rounded-md bg-gray-50 w-11/12 mx-4 px-1 py-0.5 font-extralight text-gray-600 active:border-blue-500 placeholder:italic placeholder:text-gray-500"
           />
         </div>
-        <button type="submit">Login</button>
-      </form>
-    </>
+      </div>
+      <button type="submit" className="btn">Login</button>
+    </form>
+    </div>
   );
 }
 
