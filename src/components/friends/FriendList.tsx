3d5f94786311e0eb3f812de27847066130bb16fc
Daniel Zych <danjzych@gmail.com>
2023-11-27 14:16:19 -0600

    Added empty state for friendslist

--- src/components/friends/FriendList.tsx
+++ src/components/friends/FriendList.tsx
@@ -1,12 +1,26 @@
+import { Link } from "react-router-dom";
 import FriendCard from "./FriendCard";
 import IsLoading from "../common/IsLoading";
 
 function FriendList({ nearbyUsers, isUsersLoaded, rateUser }) {
 
-  if (nearbyUsers !== null && nearbyUsers.length === 0){
+  if (nearbyUsers !== null && nearbyUsers?.length === 0){
     return (
-      <div className="absolute flex justify-center items-center h-screen w-screen bg-inherit">
-        <p>You have run out of people to rate =(</p>
+      <div className="absolute flex flex-col justify-center items-center h-screen w-screen bg-inherit">
+        <p className="text-lg font-semibold">
+            Looks like there's no friends to explore right now.
+        </p>
+        <p className=" py-4 text-md">
+            Go check out your matches, or come back soon to see who is in your area!
+        </p>
+        <small className="pb-4 text-neutral-400">
+          Or, for testing purposes, set your <b>location</b> to <em>10000</em> and <b>radius</b> to <em>10</em> to see our seed users.
+        </small>
+        <button className="btn btn-secondary shadow-2xl">
+            <Link to="/matches">
+                Matches
+            </Link>
+        </button>
       </div>
     )
   }
